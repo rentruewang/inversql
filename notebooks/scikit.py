54a5f0dea1068575577379f03f73ca345bf76c44
RenChu Wang <patrick1031wang@gmail.com>
2026-05-26 10:16:53 -0700

    🛤️ Decision tree boolean paths (#9)
    Implement `.predict`, compare against sklearn's predict function.
    
    Fix #10

---
+++ notebooks/scikit.py
@@ -0,0 +1,144 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.3
+#   kernelspec:
+#     display_name: .venv
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# ## The notebook copied from scikit learn, with some modifications
+
+# %%
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn import datasets, model_selection, tree
+
+# %%
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+X_train, X_test, y_train, y_test = model_selection.train_test_split(
+    X, y, random_state=0
+)
+
+clf = tree.DecisionTreeClassifier(random_state=0)
+clf.fit(X_train, y_train)
+
+# %%
+n_nodes = clf.tree_.node_count
+children_left = clf.tree_.children_left
+children_right = clf.tree_.children_right
+feature = clf.tree_.feature
+threshold = clf.tree_.threshold
+values = clf.tree_.value
+
+{
+    "n_nodes": n_nodes,
+    "children_left": children_left,
+    "children_right": children_right,
+    "feature": feature,
+    "threshold": threshold,
+    "values": values,
+}
+
+# %%
+node_depth = np.zeros(shape=n_nodes, dtype=np.int64)
+is_leaves = np.zeros(shape=n_nodes, dtype=bool)
+stack = [(0, 0)]  # start with the root node id (0) and its depth (0)
+while len(stack) > 0:
+    # `pop` ensures each node is only visited once
+    node_id, depth = stack.pop()
+    node_depth[node_id] = depth
+
+    # If the left and right child of a node is not the same we have a split
+    # node
+    is_split_node = children_left[node_id] != children_right[node_id]
+    # If a split node, append left and right children and depth to `stack`
+    # so we can loop through them
+    if is_split_node:
+        stack.append((children_left[node_id], depth + 1))
+        stack.append((children_right[node_id], depth + 1))
+    else:
+        is_leaves[node_id] = True
+
+print(
+    "The binary tree structure has {n} nodes and has "
+    "the following tree structure:\n".format(n=n_nodes)
+)
+for i in range(n_nodes):
+    if is_leaves[i]:
+        print(
+            "{space}node={node} is a leaf node with value={value}.".format(
+                space=node_depth[i] * "\t", node=i, value=np.around(values[i], 3)
+            )
+        )
+    else:
+        print(
+            "{space}node={node} is a split node with value={value}: "
+            "go to node {left} if X[:, {feature}] <= {threshold} "
+            "else to node {right}.".format(
+                space=node_depth[i] * "\t",
+                node=i,
+                left=children_left[i],
+                feature=feature[i],
+                threshold=threshold[i],
+                right=children_right[i],
+                value=np.around(values[i], 3),
+            )
+        )
+
+# %%
+tree.plot_tree(clf, proportion=True)
+plt.show()
+
+# %%
+threshold
+
+# %%
+node_indicator = clf.decision_path(X_test)
+node_indicator.shape
+
+# %%
+leaf_id = clf.apply(X_test)
+leaf_id
+
+# %%
+sample_id = 0
+# obtain ids of the nodes `sample_id` goes through, i.e., row `sample_id`
+node_index = node_indicator.indices[
+    node_indicator.indptr[sample_id] : node_indicator.indptr[sample_id + 1]
+]
+
+print("Rules used to predict sample {id}:\n".format(id=sample_id))
+for node_id in node_index:
+    # continue to the next node if it is a leaf node
+    if leaf_id[sample_id] == node_id:
+        continue
+
+    # check if value of the split feature for sample 0 is below threshold
+    if X_test[sample_id, feature[node_id]] <= threshold[node_id]:
+        threshold_sign = "<="
+    else:
+        threshold_sign = ">"
+
+    print(
+        "decision node {node} : (X_test[{sample}, {feature}] = {value}) "
+        "{inequality} {threshold})".format(
+            node=node_id,
+            sample=sample_id,
+            feature=feature[node_id],
+            value=X_test[sample_id, feature[node_id]],
+            inequality=threshold_sign,
+            threshold=threshold[node_id],
+        )
+    )
+
+# %%
+print(tree.export_text(clf))
