036c355a50fd5c52d5ea73637920debfdb9376c5
RenChu Wang <patrick1031wang@gmail.com>
2026-05-31 17:43:51 -0700

    📑 Parse `sympy` expression and construct `Expr`. (#19)
    Fix #18

--- src/inversql/exprs.py
+++ src/inversql/exprs.py
@@ -3,7 +3,9 @@
 import abc
 import dataclasses as dcls
 import enum
+import functools
 import operator
+import re
 import typing
 from collections import abc as cabc
 
@@ -19,16 +21,27 @@ __all__ = [
     "OrExpr",
     "DontCareExpr",
     "feature_name",
+    "parse_feature_name",
 ]
+
+
+_FEATURE_REGEX = re.compile(r"feature_(\d+)")
 
 
 def feature_name(idx: int) -> str:
     return f"feature_{idx}"
 
 
-@typing.dataclass_transform(frozen_default=True)
+def parse_feature_name(name: str) -> int:
+    if m := _FEATURE_REGEX.match(name):
+        return int(m.group(1))
+
+    raise ValueError("Cannot parse the feature.")
+
+
+@typing.dataclass_transform()
 def expr_dcls(cls):
-    return dcls.dataclass(frozen=True)(cls)
+    return dcls.dataclass()(cls)
 
 
 @expr_dcls
@@ -44,18 +57,18 @@ class Expr(abc.ABC):
 
     def __and__(self, other: Expr) -> Expr:
         "`self & other`"
-        return AndExpr(self, other)
+        return AndExpr(left=self, right=other)
 
     def __or__(self, other: Expr) -> Expr:
         "`self | other`"
-        return OrExpr(self, other)
+        return OrExpr(left=self, right=other)
 
     @abc.abstractmethod
     def eval(self, sample: FloatArray) -> bool:
         "Evaluate with `sample` to give `True` or `False`."
         raise NotImplementedError
 
-    def to_sympy(self, simplify: bool) -> sympy.Expr:
+    def to_sympy(self, simplify: bool = False) -> sympy.Expr:
         expr = self._to_sympy(simplify)
 
         if simplify:
@@ -67,6 +80,54 @@ class Expr(abc.ABC):
     def _to_sympy(self, simplify: bool) -> sympy.Expr:
         "Convert to a `sympy.Expr`."
         raise NotImplementedError
+
+
+@typing.no_type_check
+def parse_sympy_expr(expr: sympy.Expr) -> Expr:
+    """
+    Parse the given sympy expression.
+    Convert the `sympy.Expr` into our `Expr` (only AND / OR / comparison),
+    this is fine because those are the only features we use from `sympy`.
+
+    Due to our limitation (that our expressions are binary), AND / OR are `fold`ed.
+
+    At some point we might transition to full `sympy`.
+    """
+
+    args = expr.args
+
+    match expr:
+        case sympy.And():
+            return functools.reduce(AndExpr, map(parse_sympy_expr, args))
+        case sympy.Or():
+            return functools.reduce(OrExpr, map(parse_sympy_expr, args))
+
+        # We don't have a `NotExpr`, but directly invoke the `__invert__`.
+        case sympy.Not():
+            [expr] = args
+
+            return ~parse_sympy_expr(expr)
+
+        case (
+            sympy.Eq() | sympy.Ne() | sympy.Ge() | sympy.Gt() | sympy.Le() | sympy.Lt()
+        ):
+            left, right = args
+            assert isinstance(left, sympy.Symbol), left
+            return CmpExpr(
+                feat_idx=parse_feature_name(str(left)),
+                cmp=CmpOp.from_sympy_type(type(expr)),
+                threshold=float(right),
+            )
+
+    raise ValueError(f"Unsupported expression: {expr}")
+
+
+def _is_expr_list(args, /) -> typing.TypeIs[cabc.Sequence[sympy.Expr]]:
+    if not isinstance(args, cabc.Sequence):
+        return False
+    if any(not isinstance(arg, sympy.Expr) for arg in args):
+        return False
+    return True
 
 
 @expr_dcls
@@ -104,7 +165,7 @@ class CmpOp(enum.StrEnum):
     LE = "<="
     LT = "<"
 
-    def __invert__(self):
+    def __invert__(self) -> CmpOp:
         match self:
             case CmpOp.EQ:
                 return CmpOp.NE
@@ -135,6 +196,44 @@ class CmpOp(enum.StrEnum):
             case CmpOp.LT:
                 return operator.lt
 
+    @property
+    def sympy_type(self):
+        match self:
+            case CmpOp.EQ:
+                return sympy.Eq
+            case CmpOp.NE:
+                return sympy.Ne
+            case CmpOp.GE:
+                return sympy.Ge
+            case CmpOp.GT:
+                return sympy.Gt
+            case CmpOp.LE:
+                return sympy.Le
+            case CmpOp.LT:
+                return sympy.Lt
+
+    @classmethod
+    def from_sympy_type(cls, typ: type[sympy.Expr]) -> CmpOp:
+        if typ == sympy.Eq:
+            return cls.EQ
+
+        if typ == sympy.Ne:
+            return cls.NE
+
+        if typ == sympy.Ge:
+            return cls.GE
+
+        if typ == sympy.Gt:
+            return cls.GT
+
+        if typ == sympy.Le:
+            return cls.LE
+
+        if typ == sympy.Lt:
+            return cls.LT
+
+        raise ValueError(f"Operator type {typ} has no `CmpOp` correspondence.")
+
 
 @expr_dcls
 class CmpExpr(Expr):
@@ -156,7 +255,7 @@ class CmpExpr(Expr):
         if not isinstance(self.cmp, CmpOp):
             raise TypeError(f"{self.cmp=} should be `CmpOp`, got {type(self.cmp)=}.")
 
-        if not isinstance(self.threshold, float):
+        if not isinstance(self.threshold, int | float):
             raise TypeError(f"{self.threshold=} should be float.")
 
     @typing.override
@@ -174,7 +273,7 @@ class CmpExpr(Expr):
     @typing.override
     def _to_sympy(self, simplify: bool) -> sympy.Expr:
         symbol = sympy.Symbol(feature_name(self.feat_idx))
-        return self.cmp.op(symbol, self.threshold)
+        return self.cmp.sympy_type(symbol, self.threshold)
 
 
 @expr_dcls
@@ -194,6 +293,13 @@ class AndExpr(Expr):
     @typing.override
     def __invert__(self) -> Expr:
         return ~self.left or ~self.right
+
+    def __eq__(self, other: object):
+        if isinstance(other, AndExpr):
+            in_order = self.left == other.left and self.right == other.right
+            inverted = self.left == other.right and self.right == other.left
+            return in_order or inverted
+        return NotImplemented
 
     @typing.override
     def eval(self, sample: FloatArray) -> bool:
@@ -225,6 +331,13 @@ class OrExpr(Expr):
     @typing.override
     def __invert__(self) -> Expr:
         return ~self.left and ~self.right
+
+    def __eq__(self, other: object):
+        if isinstance(other, OrExpr):
+            in_order = self.left == other.left and self.right == other.right
+            inverted = self.left == other.right and self.right == other.left
+            return in_order or inverted
+        return NotImplemented
 
     @typing.override
     def eval(self, sample: FloatArray) -> bool:
